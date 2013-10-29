adfff3dcd90c551e1817e747648210f1169ba698
Robin Swenson-Healey <rjswenson@gmail.com>
2013-10-29 13:01:32 -0700

    tests created for roles, users, and login/out

--- test/features/logging/sign_in_test.rb
+++ test/features/logging/sign_in_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 feature "Sign in" do
-  scenario "the link should work" do
-    skip
+  scenario "with valid information" do
+    visit root_path
+    click_link("Sign in")
+    fill_in "Email", with: users(:basic).email
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+
+    page.text.must_include "Signed in successfully."
+    page.text.must_include "Sign out"
+    page.text.wont_include "Sign in"
+  end
+  scenario "fails with invalid info" do
+    visit root_path
+    click_link("Sign in")
+    fill_in "Email", with: users(:basic).email
+    fill_in "Password", with: "NotThePassword"
+    click_button "Sign in"
+
+    page.text.wont_include "Signed in successfully."
+    page.text.must_include "Sign in"
+    page.text.wont_include "Sign out"
+    page.text.must_include "Invalid"
   end
 end
