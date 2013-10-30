bff4a784f9116acbd22190bd2ff5b4231868bc13
SG <sy1840@gmail.com>
2013-10-30 14:02:59 -0700

    better flash notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,14 @@ module ApplicationHelper
   def resource_class
     devise_mapping.to
   end
+
+  def flash_class(level)
+    case level
+        when :notice then "alert alert-info"
+        when :success then "alert alert-success"
+        when :error then "alert alert-error"
+        when :alert then "alert alert-error"
+    end
+  end
 end
+
