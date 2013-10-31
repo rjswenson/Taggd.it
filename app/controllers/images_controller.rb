30b5399bc1a9e56cd08ca4180b0c920e41a5a2fd
SG <sy1840@gmail.com>
2013-10-31 11:42:05 -0700

    add except to images controller for index page

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index]
   # GET /images
   # GET /images.json
   def index
