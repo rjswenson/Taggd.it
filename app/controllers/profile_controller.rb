f893d9191022217918b8032ab9fe19d62da51d1e
Robin Swenson-Healey <rjswenson@gmail.com>
2013-10-31 15:01:58 -0700

    individual profiles created for each user

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,7 +1,11 @@
 class ProfileController < ApplicationController
 before_filter :authenticate_user!
-def index
-  @images = Image.all
-end
+  def index
+    @images = Image.all
+  end
 
+  def show
+    @user = User.find(request.original_url.split('/').last)
+    @images = @user.images
+  end
 end
