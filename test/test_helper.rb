b0dc36382f37612243338d9897a25b2855dd8bb2
SG <sy1840@gmail.com>
2013-11-01 10:31:53 -0700

    add within line to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,8 +24,9 @@ end
 
 def sign_in(user)
   visit new_user_session_path
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: "password"
-  click_button "Sign in"
+  within(".signup-box") do
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+  end
 end
-
