c3a2bf877f8cb83b63f44f8876b95285258f8647
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-01 12:44:25 -0700

    tests and modals work together now

--- test/features/roles/logged_user_role_test.rb
+++ test/features/roles/logged_user_role_test.rb
@@ -3,10 +3,12 @@ require "test_helper"
 feature "a newly signed up visitor" do
   scenario "has a role of user" do
     visit new_user_registration_path
-    fill_in "Email", with: "random123@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button("Join Us!")
+    within(:css, "div.modal-signup-box") do
+      fill_in "Email", with: "new1@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_on "Join us"
+    end
 
     new_guy = User.last
     assert_equal new_guy.role, "user"
