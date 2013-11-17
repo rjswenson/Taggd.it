f57e42568b6392a98560b5f030b14560f382daa9
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-16 17:10:02 -0800

    assets forced compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ TaggdIt::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.enabled = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
