6f2cd726824db41fffaa3d1cd1bda8af0217a520
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-29 11:48:22 -0800

    remove twitter overlap in BOTH initializers, fixes #28

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,12 @@
 TaggdIt::Application.routes.draw do
 
-  # scope ":locale" do
+  devise_for :users, path_names: { sign_in: "login", sign_out: "logout" },
+              controllers: { omniauth_callbacks: "omniauth_callbacks"}
 
-    devise_for :users, path_names: { sign_in: "login", sign_out: "logout" },
-                controllers: { omniauth_callbacks: "omniauth_callbacks"}
-                # skip: :omniauth_callbacks
-
-    resources :images
-    resources :profile
-    match '/about',     to: 'static_pages#about',   via: 'get'
-    match '/policy',    to: 'static_pages#policy',  via: 'get'
-  # end
-
-  # match "/users/auth/:provider",
-  #   contstraints: { provider: /Alinkein|reddit|facebook|googlez/ },
-  #   to: "devise/omniauth_callbacks#passthru",
-  #   as: "omniauth_authorize",
-  #   via: [:get, :post]
-
-  # match "/users/auth/:action/callback",
-  #   contstraints: { action: /Alinkein|reddit|facebook|googlez/ },
-  #   to: "devise/omniauth_callbacks",
-  #   as: "omniauth_callback",
-  #   via: [:get, :post]
+  resources :images
+  resources :profile
+  match '/about',     to: 'static_pages#about',   via: 'get'
+  match '/policy',    to: 'static_pages#policy',  via: 'get'
 
   root 'images#index'
 end
