7c5cf60414c7b442d8c87766ce971fffea2e8bd0
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-29 18:10:50 -0800

    post as member of images

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ TaggdIt::Application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" },
               controllers: { omniauth_callbacks: "omniauth_callbacks"}
 
-  resources :images
+  resources :images do
+    member { post :vote }
+  end
+
   resources :profile
   match '/about',     to: 'static_pages#about',   via: 'get'
   match '/policy',    to: 'static_pages#policy',  via: 'get'
