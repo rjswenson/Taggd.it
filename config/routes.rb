99703214f55a77067f4249af1a7949f63ab852e3
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-30 13:50:44 -0800

    reformat for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ TaggdIt::Application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" },
               controllers: { omniauth_callbacks: "omniauth_callbacks"}
 
+  resources :profile
   resources :images do
     member { post :vote }
   end
 
-  resources :profile
-  match '/about',     to: 'static_pages#about',   via: 'get'
-  match '/policy',    to: 'static_pages#policy',  via: 'get'
+  match '/about',       to: 'static_pages#about',   via: :get
+  match '/policy',      to: 'static_pages#policy',  via: :get
 
   root 'images#index'
 end
