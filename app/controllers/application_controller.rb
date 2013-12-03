ee83390f685a903578c559d5372f5857257c08ea
SG <sy1840@gmail.com>
2013-12-03 11:16:15 -0800

    add profile page, create username, edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up)
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 
   def set_locale
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131201204011) do
+ActiveRecord::Schema.define(version: 20131203180912) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -99,9 +99,11 @@ ActiveRecord::Schema.define(version: 20131201204011) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
