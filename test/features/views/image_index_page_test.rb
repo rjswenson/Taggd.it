9ff7eed086e021dfd2f0fbf2d9d0d09f18c1f26c
Robin Swenson-Healey <rjswenson@gmail.com>
2013-12-04 12:24:10 -0800

    image index testing

--- test/features/views/image_index_page_test.rb
+++ test/features/views/image_index_page_test.rb
@@ -1,1 +1,89 @@
+require "test_helper"
 
+feature "On the index page, visitors" do
+  scenario "see images" do
+   visit images_path
+   page.text.must_include(images(:smile).location)
+  end
+
+  scenario "see votes" do
+    visit images_path
+    page.text.must_include("votes")
+    page.text.must_include("0")
+  end
+
+  scenario "cannot vote" do
+    visit images_path
+    find('.vote-up').click
+    page.text.must_include("You need to sign in")
+  end
+
+  scenario "cannot upload via modal" do
+    visit images_path
+    click_on "Upload"
+    page.text.must_include("Sign up")
+  end
+
+  scenario "cannot upload via new page" do
+    visit new_image_path
+    page.text.must_include("You need to sign in")
+  end
+end
+
+feature "On index page, user" do
+  scenario "Can vote" do
+    sign_in(users(:basic))
+    visit images_path
+    find('.vote-down').click
+    page.text.must_include("-1")
+  end
+  scenario "Can't Delete" do
+    sign_in(users(:basic))
+    visit images_path
+    page.text.wont_include("Destroy")
+  end
+
+  scenario "see images" do
+    sign_in(users(:basic))
+    visit images_path
+    page.text.must_include(images(:smile).location)
+  end
+
+  scenario "see votes" do
+    sign_in(users(:basic))
+    visit images_path
+    page.text.must_include("votes")
+    page.text.must_include("0")
+  end
+end
+
+feature "On index page, admin" do
+  scenario "Can vote" do
+    sign_in(users(:admin))
+    visit images_path
+    find('.vote-down').click
+    page.text.must_include("-1")
+  end
+
+  scenario "Can delete" do
+    sign_in(users(:admin))
+    visit images_path
+    page.text.must_include(images(:smile).location)
+    click_on "Destroy"
+    page.text.wont_include(images(:smile).location)
+    page.text.must_include("successfully destroyed")
+  end
+
+  scenario "see images" do
+    sign_in(users(:admin))
+    visit images_path
+    page.text.must_include(images(:smile).location)
+  end
+
+  scenario "see votes" do
+    sign_in(users(:admin))
+    visit images_path
+    page.text.must_include("votes")
+    page.text.must_include("0")
+  end
+end
